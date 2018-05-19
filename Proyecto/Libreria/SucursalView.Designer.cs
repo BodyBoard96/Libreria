2b7455ba692e7b852fc7347a34af8194930ff22d
juan325 <bachillerato_juancarlos@hotmail.com>
2018-05-18 21:51:15 -0600

    juanca: finalizado formulario sucursal

--- Proyecto/Libreria/SucursalView.Designer.cs
+++ Proyecto/Libreria/SucursalView.Designer.cs
@@ -28,11 +28,153 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtCorreo = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtTelefono = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtDireccion = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dtgSucursal = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgSucursal)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Click += new System.EventHandler(this.btnModificar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // txtCorreo
+            // 
+            this.txtCorreo.Location = new System.Drawing.Point(556, 67);
+            this.txtCorreo.Name = "txtCorreo";
+            this.txtCorreo.Size = new System.Drawing.Size(183, 20);
+            this.txtCorreo.TabIndex = 52;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(470, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 51;
+            this.label3.Text = "Email:";
+            // 
+            // txtTelefono
+            // 
+            this.txtTelefono.Location = new System.Drawing.Point(556, 30);
+            this.txtTelefono.Name = "txtTelefono";
+            this.txtTelefono.Size = new System.Drawing.Size(183, 20);
+            this.txtTelefono.TabIndex = 50;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(470, 41);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(52, 13);
+            this.label6.TabIndex = 49;
+            this.label6.Text = "Telefono:";
+            // 
+            // txtDireccion
+            // 
+            this.txtDireccion.Location = new System.Drawing.Point(190, 77);
+            this.txtDireccion.Name = "txtDireccion";
+            this.txtDireccion.Size = new System.Drawing.Size(183, 20);
+            this.txtDireccion.TabIndex = 48;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(68, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 47;
+            this.label2.Text = "Direccion:";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(190, 34);
+            this.txtId.Name = "txtId";
+            this.txtId.ReadOnly = true;
+            this.txtId.Size = new System.Drawing.Size(183, 20);
+            this.txtId.TabIndex = 46;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(68, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 45;
+            this.label1.Text = "Identificador: ";
+            // 
+            // dtgSucursal
+            // 
+            this.dtgSucursal.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgSucursal.Location = new System.Drawing.Point(15, 277);
+            this.dtgSucursal.Name = "dtgSucursal";
+            this.dtgSucursal.Size = new System.Drawing.Size(880, 331);
+            this.dtgSucursal.TabIndex = 53;
+            // 
+            // SucursalView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(907, 620);
+            this.Controls.Add(this.dtgSucursal);
+            this.Controls.Add(this.txtCorreo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtTelefono);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtDireccion);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.label1);
+            this.Name = "SucursalView";
             this.Text = "SucursalView";
+            this.Controls.SetChildIndex(this.btnSalir, 0);
+            this.Controls.SetChildIndex(this.btnAgregar, 0);
+            this.Controls.SetChildIndex(this.btnModificar, 0);
+            this.Controls.SetChildIndex(this.btnEliminar, 0);
+            this.Controls.SetChildIndex(this.btnLimpiar, 0);
+            this.Controls.SetChildIndex(this.label1, 0);
+            this.Controls.SetChildIndex(this.txtId, 0);
+            this.Controls.SetChildIndex(this.label2, 0);
+            this.Controls.SetChildIndex(this.txtDireccion, 0);
+            this.Controls.SetChildIndex(this.label6, 0);
+            this.Controls.SetChildIndex(this.txtTelefono, 0);
+            this.Controls.SetChildIndex(this.label3, 0);
+            this.Controls.SetChildIndex(this.txtCorreo, 0);
+            this.Controls.SetChildIndex(this.dtgSucursal, 0);
+            ((System.ComponentModel.ISupportInitialize)(this.dtgSucursal)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtCorreo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtTelefono;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtDireccion;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dtgSucursal;
     }
 }
--- Proyecto/Libreria/SucursalView.cs
+++ Proyecto/Libreria/SucursalView.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Controller;
+using Model;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,127 @@ namespace Libreria
 {
     public partial class SucursalView : FormTemplate
     {
+        SucursalService clS = null;
+        Sucursal cl = null;
         public SucursalView()
         {
             InitializeComponent();
+            allSucursal();//llamando al método para llenar DataGridView
+        }
+        /**
+        * Método para cargar todas las sucursales de la base
+        * **/
+        public void allSucursal()
+        {
+            clS = new SucursalService();
+            dtgSucursal.DataSource = clS.allSucursales();
+        }
+        /**
+         * Método para insertar sucursales en la base
+         * **/
+        private void addSucursal(Sucursal sucursal)
+        {
+            MessageBox.Show(clS.insert(sucursal));
+            allSucursal();
+            limpiar();
+        }
+        /**
+         * Método para modificar sucursales en la base
+         * **/
+        private void updateSucursal(Sucursal sucursal)
+        {
+            MessageBox.Show(clS.update(sucursal));
+            allSucursal();
+            limpiar();
+        }
+        /**
+         * Método para eliminar sucursales en la base
+         * **/
+        private void deleteSucursal(int sucursal)
+        {
+            MessageBox.Show(clS.delete(sucursal));
+            allSucursal();
+            limpiar();
+        }
+        public override void limpiar()
+        {
+            txtId.Text = "";
+            txtDireccion.Text = "";
+            txtTelefono.Text = "";
+            txtCorreo.Text = "";
+        }
+        private Boolean validate()
+        {
+            if ("".Equals(txtDireccion.Text) ||
+            "".Equals(txtTelefono.Text) ||
+            "".Equals(txtCorreo.Text))
+            {
+                return true;
+            }
+            return false;
+        }
+        private Sucursal llenarSucursal(Boolean v)
+        {
+            Sucursal obj = new Sucursal();
+            if (v)
+                obj.CodSucursal = int.Parse(txtId.Text);
+            obj.Direccion = txtDireccion.Text;
+            obj.Telefono = txtTelefono.Text;
+            obj.Email = txtCorreo.Text;
+            return obj;
+        }
+        private void dtgProveedor_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txtId.Text = dtgSucursal.Rows[e.RowIndex].Cells[0].Value.ToString().Replace(" ", "");
+            txtDireccion.Text = dtgSucursal.Rows[e.RowIndex].Cells[1].Value.ToString().Replace(" ", "");
+            txtTelefono.Text = dtgSucursal.Rows[e.RowIndex].Cells[2].Value.ToString().Replace(" ", "");
+            txtCorreo.Text = dtgSucursal.Rows[e.RowIndex].Cells[3].Value.ToString().Replace(" ", "");
+            btnAgregar.Enabled = false;
+            btnModificar.Enabled = true;
+            btnEliminar.Enabled = true;
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            if (!validate())
+            {
+                addSucursal(llenarSucursal(false));
+            }
+        }
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+            if (!validate())
+            {
+                updateSucursal(llenarSucursal(true));
+                btnAgregar.Enabled = true;
+                btnModificar.Enabled = false;
+                btnEliminar.Enabled = false;
+            }
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (MessageBox.Show("¿Desea eliminar el registro?", "Aviso", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1) == DialogResult.Yes)
+                {
+                    deleteSucursal(int.Parse(txtId.Text.Replace(" ", "")));
+                    btnAgregar.Enabled = true;
+                    btnModificar.Enabled = false;
+                    btnEliminar.Enabled = false;
+
+                }
+            }
+            catch (Exception i)
+            {
+                MessageBox.Show("Ocurrio un problema, favor intentar mas tarde");
+            }
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
