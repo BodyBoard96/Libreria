514d94c8ed9afee36c35d1e64ad4b2dedeac5c0d
CesarJRomero <cesarromero.cr3@gmail.com>
2018-06-02 12:54:26 -0600

    actualizados

--- Proyecto/Libreria/PresentacionView.cs
+++ Proyecto/Libreria/PresentacionView.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Controller;
+using Model;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,138 @@ namespace Libreria
 {
     public partial class PresentacionView : FormTemplate
     {
+        PresentacionService clS = null;
+        Presentacion cl = null;
         public PresentacionView()
         {
             InitializeComponent();
+            allPresentacion();//llamando al método para llenar DataGridView
+        }
+
+         /**
+         * Método para cargar todos los presentacion de la base
+         * **/
+        public void allPresentacion()
+        {
+            clS = new PresentacionService();
+            dtgPresentacion.DataSource = clS.allSucursales();
+
+        }
+
+
+        /**
+         * Método para insertar presentacion en la base
+         * **/
+        private void addPresentacion(Presentacion pre)
+        {
+            MessageBox.Show(clS.insert(pre));
+            allPresentacion();
+            limpiar();
+        }
+
+        /**
+        * Método para modificar presentacion en la base
+        * **/
+        private void updatePresentacion(Presentacion pre)
+        {
+            MessageBox.Show(clS.update(pre));
+            allPresentacion();
+            limpiar();
+        }
+
+        /**
+         * Método para eliminar presentacion en la base
+         * **/
+        private void deletePresentacion(int pre)
+        {
+            MessageBox.Show(clS.delete(pre));
+            allPresentacion();
+            limpiar();
+        }
+
+        public override void limpiar()
+        {
+            txtcod_pre.Text = "";
+            txtdescripcion_pre.Text = "";
+            txtcan_uni_pre.Text = "";
+
+        }
+        private Boolean validate()
+        {
+            if ("".Equals(txtcod_pre.Text) ||
+            "".Equals(txtdescripcion_pre.Text) ||
+            "".Equals(txtcan_uni_pre.Text))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private Clientes llenarPresentacion(Boolean v)
+        {
+            Presentacion obj = new Presentacion();
+            if (v)
+                obj.cod_pre = int.Parse(txCod_pre.Text);
+            obj.Descripcion = txtdescripcion_pre.Text;
+            obj.Can_uni_pro = txtcan_uni_pre.Text = "";
+
+
+            return obj;
+        }
+
+        private void dtgPresentacion_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+            txtcod_pre.Text = dtgPresentacion.Rows[e.RowIndex].Cells[0].Value.ToString().Replace(" ", "");
+            txtdescripcion_pre.Text.Text = dtgPresentacion.Rows[e.RowIndex].Cells[1].Value.ToString().Replace(" ", "");
+            txtcan_uni_pre.Text = dtgPresentacion.Rows[e.RowIndex].Cells[2].Value.ToString().Replace(" ", "");
+           
+            btnAgregar.Enabled = false;
+            btnModificar.Enabled = true;
+            btnEliminar.Enabled = true;
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            if (!validate())
+            {
+                addPresentacion(llenarPresentacion(false));
+            }
+        }
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+
+            if (!validate())
+            {
+                updatePresentacion(llenarPresentacion(true));
+                btnAgregar.Enabled = true;
+                btnModificar.Enabled = false;
+                btnEliminar.Enabled = false;
+            }
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (MessageBox.Show("¿Desea eliminar el pago?", "Aviso", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1) == DialogResult.Yes)
+                {
+                    deletePresentacion(int.Parse(txtId.Text.Replace(" ", "")));
+                    btnAgregar.Enabled = true;
+                    btnModificar.Enabled = false;
+                    btnEliminar.Enabled = false;
+
+                }
+            }
+            catch (Exception i)
+            {
+                MessageBox.Show("Ocurrio un problema, favor intentar mas tarde");
+            }
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
--- Proyecto/Libreria/TipoPagoView.cs
+++ Proyecto/Libreria/TipoPagoView.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Controller;
+using Model;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,114 @@ namespace Libreria
 {
     public partial class TipoPagoView : FormTemplate
     {
+        TipoPagoService clS = null;
+        TipoPago cl = null;
         public TipoPagoView()
         {
             InitializeComponent();
+            allTipoPago(); //llamando al método para llenar DataGridView
+        }
+        /**
+         * Método para cargar todos los tipo pago de la base
+         * **/
+        public void allTipoPago()
+        {
+            clS = new TipoPagoService();
+            dtgTipoPago.DataSource = clS.allSucursales();
+        }
+        /**
+         * Método para insertar tipo pago en la base
+         * **/
+        private void addTipoPago(TipoPago Pago)
+        {
+            MessageBox.Show(clS.insert(Pago));
+            allTipoPago();
+            limpiar();
+        }
+
+        /**
+         * Método para modificar tipo pago en la base
+         * **/
+        private void updateTipoPAgo(TipoPago Pago)
+        {
+            MessageBox.Show(clS.update(Pago));
+            allTipoPago();
+            limpiar();
+        }
+
+        /**
+     * Método para eliminar tipo pago en la base
+     * **/
+        private void deleteTipoPago(int Pago)
+        {
+            MessageBox.Show(clS.delete(Pago));
+            allTipoPago();
+            limpiar();
+        }
+
+        public override void limpiar()
+        {
+            txtId.Text = "";
+            txtTipo_pago.Text = "";
+        }
+
+        private Boolean validate()
+        {
+            if ("".Equals(txtTipo_pago.Text))
+            {
+                
+                return true ; 
+            }
+            return false ;
+        }
+
+        private TipoPago llenartipo_pago(Boolean v)
+        {
+            TipoPago obj = new TipoPago();
+            if (v)
+                obj.cod_tipo_pago = int.Parse(txttipo_pago.Text)
+                obj.CodigoTipoPago = txtTipo_pago.Text = "";
+            return obj;
+        }
+
+        private void dtgTipoPago_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+            txtId.Text = dtgTipoPago.Rows[e.RowIndex].Cells[0].Value.ToString().Replace(" ", "");
+            btnAgregar.Enabled = false;
+            btnModificar.Enabled = true;
+            btnEliminar.Enabled = true;
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            if (!validate())
+            {
+                addTipoPago(llenartipo_pago(false));
+            }
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (MessageBox.Show("¿Desea eliminar el Pago?", "Aviso", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1) == DialogResult.Yes)
+                {
+                    deleteTipoPago(int.Parse(txtId.Text.Replace(" ", "")));
+                    btnAgregar.Enabled = true;
+                    btnModificar.Enabled = false;
+                    btnEliminar.Enabled = false;
+
+                }
+            }
+            catch (Exception i)
+            {
+                MessageBox.Show("Ocurrio un problema, favor intentar mas tarde");
+            }
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
