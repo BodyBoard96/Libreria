857af87ae6f8cdb03e962080c5cc64e3a305894a
juan325 <bachillerato_juancarlos@hotmail.com>
2018-06-02 16:48:15 -0600

    juanca: avance producto

--- Proyecto/Libreria/ProductoView.Designer.cs
+++ Proyecto/Libreria/ProductoView.Designer.cs
@@ -28,11 +28,235 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtPrecio_venta = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtNombre_Producto = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtCod_presentacion = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.label = new System.Windows.Forms.Label();
+            this.dtgProductos = new System.Windows.Forms.DataGridView();
+            this.txtPre_compra = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.CmbCod_sucursal = new System.Windows.Forms.ComboBox();
+            this.cmbCod_Prov = new System.Windows.Forms.ComboBox();
+            this.cmbCod_Color = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgProductos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(376, 145);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(85, 13);
+            this.label8.TabIndex = 50;
+            this.label8.Text = "Codigo sucursal:";
+            // 
+            // txtPrecio_venta
+            // 
+            this.txtPrecio_venta.Location = new System.Drawing.Point(494, 102);
+            this.txtPrecio_venta.Name = "txtPrecio_venta";
+            this.txtPrecio_venta.Size = new System.Drawing.Size(183, 20);
+            this.txtPrecio_venta.TabIndex = 49;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(376, 109);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(85, 13);
+            this.label9.TabIndex = 48;
+            this.label9.Text = "Precio de venta:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(376, 66);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(112, 13);
+            this.label10.TabIndex = 47;
+            this.label10.Text = "Codigo de proveedor: ";
+            // 
+            // txtNombre_Producto
+            // 
+            this.txtNombre_Producto.Location = new System.Drawing.Point(164, 141);
+            this.txtNombre_Producto.Name = "txtNombre_Producto";
+            this.txtNombre_Producto.Size = new System.Drawing.Size(183, 20);
+            this.txtNombre_Producto.TabIndex = 46;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(41, 148);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 13);
+            this.label4.TabIndex = 45;
+            this.label4.Text = "Nombre Producto:";
+            // 
+            // txtCod_presentacion
+            // 
+            this.txtCod_presentacion.Location = new System.Drawing.Point(164, 106);
+            this.txtCod_presentacion.Name = "txtCod_presentacion";
+            this.txtCod_presentacion.Size = new System.Drawing.Size(183, 20);
+            this.txtCod_presentacion.TabIndex = 42;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(41, 113);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(108, 13);
+            this.label6.TabIndex = 41;
+            this.label6.Text = "Codigo Presentacion:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 13);
+            this.label2.TabIndex = 39;
+            this.label2.Text = "codigo de color:";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(164, 29);
+            this.txtId.Name = "txtId";
+            this.txtId.ReadOnly = true;
+            this.txtId.Size = new System.Drawing.Size(183, 20);
+            this.txtId.TabIndex = 38;
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(41, 29);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(117, 13);
+            this.label.TabIndex = 37;
+            this.label.Text = "Identificador Producto: ";
+            // 
+            // dtgProductos
+            // 
+            this.dtgProductos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgProductos.Location = new System.Drawing.Point(15, 280);
+            this.dtgProductos.Name = "dtgProductos";
+            this.dtgProductos.Size = new System.Drawing.Size(880, 328);
+            this.dtgProductos.TabIndex = 57;
+            // 
+            // txtPre_compra
+            // 
+            this.txtPre_compra.Location = new System.Drawing.Point(494, 22);
+            this.txtPre_compra.Name = "txtPre_compra";
+            this.txtPre_compra.Size = new System.Drawing.Size(183, 20);
+            this.txtPre_compra.TabIndex = 60;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(376, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 13);
+            this.label1.TabIndex = 59;
+            this.label1.Text = "Precio de compra:";
+            // 
+            // CmbCod_sucursal
+            // 
+            this.CmbCod_sucursal.FormattingEnabled = true;
+            this.CmbCod_sucursal.Location = new System.Drawing.Point(494, 142);
+            this.CmbCod_sucursal.Name = "CmbCod_sucursal";
+            this.CmbCod_sucursal.Size = new System.Drawing.Size(183, 21);
+            this.CmbCod_sucursal.TabIndex = 58;
+            // 
+            // cmbCod_Prov
+            // 
+            this.cmbCod_Prov.FormattingEnabled = true;
+            this.cmbCod_Prov.Location = new System.Drawing.Point(494, 62);
+            this.cmbCod_Prov.Name = "cmbCod_Prov";
+            this.cmbCod_Prov.Size = new System.Drawing.Size(183, 21);
+            this.cmbCod_Prov.TabIndex = 61;
+            // 
+            // cmbCod_Color
+            // 
+            this.cmbCod_Color.FormattingEnabled = true;
+            this.cmbCod_Color.Location = new System.Drawing.Point(164, 69);
+            this.cmbCod_Color.Name = "cmbCod_Color";
+            this.cmbCod_Color.Size = new System.Drawing.Size(183, 21);
+            this.cmbCod_Color.TabIndex = 62;
+            // 
+            // ProductoView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(907, 620);
+            this.Controls.Add(this.cmbCod_Color);
+            this.Controls.Add(this.cmbCod_Prov);
+            this.Controls.Add(this.txtPre_compra);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CmbCod_sucursal);
+            this.Controls.Add(this.dtgProductos);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtPrecio_venta);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.txtNombre_Producto);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtCod_presentacion);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.label);
+            this.Name = "ProductoView";
             this.Text = "ProductoView";
+            this.Controls.SetChildIndex(this.label, 0);
+            this.Controls.SetChildIndex(this.txtId, 0);
+            this.Controls.SetChildIndex(this.label2, 0);
+            this.Controls.SetChildIndex(this.label6, 0);
+            this.Controls.SetChildIndex(this.txtCod_presentacion, 0);
+            this.Controls.SetChildIndex(this.label4, 0);
+            this.Controls.SetChildIndex(this.txtNombre_Producto, 0);
+            this.Controls.SetChildIndex(this.label10, 0);
+            this.Controls.SetChildIndex(this.label9, 0);
+            this.Controls.SetChildIndex(this.txtPrecio_venta, 0);
+            this.Controls.SetChildIndex(this.label8, 0);
+            this.Controls.SetChildIndex(this.dtgProductos, 0);
+            this.Controls.SetChildIndex(this.CmbCod_sucursal, 0);
+            this.Controls.SetChildIndex(this.label1, 0);
+            this.Controls.SetChildIndex(this.txtPre_compra, 0);
+            this.Controls.SetChildIndex(this.btnSalir, 0);
+            this.Controls.SetChildIndex(this.btnAgregar, 0);
+            this.Controls.SetChildIndex(this.btnModificar, 0);
+            this.Controls.SetChildIndex(this.btnEliminar, 0);
+            this.Controls.SetChildIndex(this.btnLimpiar, 0);
+            this.Controls.SetChildIndex(this.cmbCod_Prov, 0);
+            this.Controls.SetChildIndex(this.cmbCod_Color, 0);
+            ((System.ComponentModel.ISupportInitialize)(this.dtgProductos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtPrecio_venta;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtNombre_Producto;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtCod_presentacion;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.DataGridView dtgProductos;
+        private System.Windows.Forms.TextBox txtPre_compra;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox CmbCod_sucursal;
+        private System.Windows.Forms.ComboBox cmbCod_Prov;
+        private System.Windows.Forms.ComboBox cmbCod_Color;
     }
 }
--- Proyecto/Libreria/ProductoView.cs
+++ Proyecto/Libreria/ProductoView.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Controller;
+using Model;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,88 @@ namespace Libreria
 {
     public partial class ProductoView : FormTemplate
     {
+        ProductoService clS = null;
+        Producto cl = null;
+
         public ProductoView()
         {
             InitializeComponent();
+            allProducto();//llamando al método para llenar DataGridView
+        }
+        /**
+        * Método para cargar todos los Productos de la base
+        * **/
+        public void allProducto()
+        {
+            clS = new ProductoService();
+            dtgProductos.DataSource = clS.allProducto();
+        }
+        /**
+         * Método para insertar productos en la base
+         * **/
+        private void addProducto(Producto producto)
+        {
+            MessageBox.Show(clS.insert(producto));
+            allProducto();
+            limpiar();
+        }
+        /**
+         * Método para modificar productos en la base
+         * **/
+        private void updateProducto(Producto producto)
+        {
+            MessageBox.Show(clS.update(producto));
+            allProducto();
+            limpiar();
+        }
+        /**
+         * Método para eliminar productos en la base
+         * **/
+        private void deleteProducto(int producto)
+        {
+            MessageBox.Show(clS.delete(producto));
+            allProducto();
+            limpiar();
+        }
+        public override void limpiar()
+        {
+            txtId.Text = "";
+            cmbCod_Color.Text = "";
+            txtCod_presentacion.Text = "";
+            txtNombre_Producto.Text = "";
+            txtPre_compra.Text = "";
+            cmbCod_Prov.Text = "";
+            txtPrecio_venta.Text = "";
+            CmbCod_sucursal.Text = "";
+        }
+        private Boolean validate()
+        {
+            if ("".Equals(cmbCod_Color.Text) ||
+            "".Equals(txtCod_presentacion.Text) ||
+            "".Equals(txtNombre_Producto.Text) ||
+            "".Equals(txtPre_compra.Text) ||
+            "".Equals(cmbCod_Prov.Text) ||
+            "".Equals(txtPrecio_venta.Text) ||
+            "".Equals(CmbCod_sucursal.Text))
+            {
+                return true;
+            }
+            return false;
+        }
+        private Producto llenarProducto(Boolean v)
+        {
+            Producto obj = new Producto();
+            if (v)
+                obj.CodProducto = int.Parse(txtId.Text);
+            obj.CodColor = txtCod_color.Text;
+            obj.CodPresentacion = txtDireccion.Text;
+            obj.CodigoBarras = txtCargo.Text;
+            obj.NombreProducto = txtTelefono.Text;
+            obj.PrecioCompra = txtCel.Text;
+            obj.CodProveedor = txtUsuario.Text;
+            obj.PrecioVenta = txtClave.Text;
+            obj.Sucursal = int.Parse(txtRol.Text);
+            return obj;
         }
     }
 }
